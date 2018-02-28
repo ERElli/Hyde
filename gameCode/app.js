a48946d4097291865a115d4a8dd819c4b038f8f8
“Moe <mmh623@mun.ca>
2018-02-28 15:34:07 -0330

    Login System added

--- gameCode/app.js
+++ gameCode/app.js
@@ -1,6 +1,12 @@
+var mongojs = require("mongojs");
+var db = mongojs('localhost:27017/myGame', ['account','progress']);
+
+
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
+
 
 app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
@@ -11,17 +17,56 @@ serv.listen(2000);
 console.log("Server started.");
 
 
+var isValidPassword = function(data,cb){
+    db.account.find({username:data.username,password:data.password},function(err,res){
+        if(res.length > 0)
+            cb(true);
+        else
+            cb(false);
+    });
+}
+var isUsernameTaken = function(data,cb){
+    db.account.find({username:data.username},function(err,res){
+        if(res.length > 0)
+            cb(true);
+        else
+            cb(false);
+    });
+}
+var addUser = function(data,cb){
+    db.account.insert({username:data.username,password:data.password},function(err){
+        cb();
+    });
+}
+
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
     console.log('socket connection');
-
-    socket.on('happy',function(data){
-        console.log('happy because ' + data.reason);
-    });
 
     socket.emit('serverMsg',{
         msg:'hello',
     });
 
+		//
+		socket.on('signIn',function(data){
+        isValidPassword(data,function(res){
+            if(res){
+                socket.emit('signInResponse',{success:true});
+            } else {
+                socket.emit('signInResponse',{success:false});
+            }
+        });
+    });
+    socket.on('signUp',function(data){
+        isUsernameTaken(data,function(res){
+            if(res){
+                socket.emit('signUpResponse',{success:false});
+            } else {
+                addUser(data,function(){
+                    socket.emit('signUpResponse',{success:true});
+                });
+            }
+        });
+    });
+
 });
- 
