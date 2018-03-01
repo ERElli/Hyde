2021e032215866945e3a03f0def7c96296ef2ac1
“Moe <mmh623@mun.ca>
2018-02-28 20:45:27 -0330

    update file

--- gameCode/app.js
+++ gameCode/app.js
@@ -1,5 +1,5 @@
-var mongojs = require("mongojs");
-var db = mongojs('localhost:27017/myGame', ['account','progress']);
+//var mongojs = require("mongojs");
+var db = null;//mongojs('localhost:27017/myGame', ['account','progress']);
 
 
 
@@ -13,30 +13,34 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 
 var isValidPassword = function(data,cb){
+  return cb(true); /*
     db.account.find({username:data.username,password:data.password},function(err,res){
         if(res.length > 0)
             cb(true);
         else
             cb(false);
-    });
+    });*/
 }
 var isUsernameTaken = function(data,cb){
+  return cb(false);
+  /*
     db.account.find({username:data.username},function(err,res){
         if(res.length > 0)
             cb(true);
         else
             cb(false);
-    });
+    });*/
 }
 var addUser = function(data,cb){
+  return cb(); /*
     db.account.insert({username:data.username,password:data.password},function(err){
         cb();
-    });
+    });*/
 }
 
 var io = require('socket.io')(serv,{});
