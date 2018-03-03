5adf3bffb3dcb5142b42e3016db3e197830451d5
“Moe <mmh623@mun.ca>
2018-03-03 18:54:40 -0330

    update db

--- gameCode/app.js
+++ gameCode/app.js
@@ -1,5 +1,5 @@
-//var mongojs = require("mongojs");
-var db = null;//mongojs('localhost:27017/myGame', ['account','progress']);
+var mongojs = require("mongojs");
+var db = mongojs('localhost:27017/myGame', ['account','progress']);
 
 
 
@@ -18,29 +18,29 @@ console.log("Server started.");
 
 
 var isValidPassword = function(data,cb){
-  return cb(true); /*
+
     db.account.find({username:data.username,password:data.password},function(err,res){
         if(res.length > 0)
             cb(true);
         else
             cb(false);
-    });*/
+    });
 }
 var isUsernameTaken = function(data,cb){
-  return cb(false);
-  /*
+
+
     db.account.find({username:data.username},function(err,res){
         if(res.length > 0)
             cb(true);
         else
             cb(false);
-    });*/
+    });
 }
 var addUser = function(data,cb){
-  return cb(); /*
+
     db.account.insert({username:data.username,password:data.password},function(err){
         cb();
-    });*/
+    });
 }
 
 var io = require('socket.io')(serv,{});
