61b8b68edde06b01e434c03f8f6299b8479cda0b
TylerSnow <33695547+TylerSnow@users.noreply.github.com>
2018-03-05 15:22:17 -0330

    Adds HUD to GUI

--- gameCode/client/interface/GUI.js
+++ gameCode/client/interface/GUI.js
@@ -32,10 +32,41 @@ GUI = function(container){
 		self.bg_ctx = self.bg.getContext("2d");
 		self.fg_ctx = self.fg.getContext("2d");
 	}
-	self.draw=function(fg_ctx){
-		fg_ctx.fillStyle="#5F8FFF";
-		fg_ctx.fillRect(0,0,self.fg.width,self.fg.height);
+	self.bgDraw=function(bg_ctx){
+		
 			
+	};
+	self.fgDraw=function(fg_ctx,playerHealth,playerMomentum,ammo){
+		var healthX=0;
+		var healthY=30;
+		var momentX=0;
+		var momentY=60;
+		var ammoX=1150;
+		var ammoY=30;
+		var weaponX=1150;
+		var weaponY=60;
+		//var weaponImg=image.jpeg;
+		
+		
+		fg_ctx.clearRect(0,0,self.fg.width,self.fg.height);
+		fg_ctx.font="18px Arial";
+		//draw bar outlines
+		fg_ctx.strokeRect(healthX,healthY,100,10);
+		fg_ctx.strokeRect(momentX,momentY,100,10);		
+		//draw Healthbar
+		fg_ctx.fillStyle="#FF0000";		
+		fg_ctx.fillRect(healthX,healthY,playerHealth,10);
+		//draw Momentumbar
+		fg_ctx.fillStyle="#0000FF";
+		fg_ctx.fillRect(momentX,momentY,playerMomentum,10);
+		//Colour Text
+		fg_ctx.fillStyle="#000000";
+		fg_ctx.fillText('Health:',healthX,healthY);
+		fg_ctx.fillText('Momentum:',momentX,momentY);
+
+		//draw ammo
+		fg_ctx.fillText('Ammo: '+ammo,ammoX,ammoY);
+		fg_ctx.fillText('Weapon: '/*+weaponImg*/,weaponX,weaponY); 	
 	};
 	return self;
 }
