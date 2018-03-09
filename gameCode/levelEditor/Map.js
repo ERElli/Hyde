8ea1ae4970897e1f78a1bbedc8722aa29e1370f5
ERElli <32199063+ERElli@users.noreply.github.com>
2018-03-09 11:08:14 -0330

    added functions to handle entity removal

--- gameCode/levelEditor/Map.js
+++ gameCode/levelEditor/Map.js
@@ -6,10 +6,11 @@ Map = function(width, height,tile_width, tile_height) {
 	self.numTilesY = height/tile_height;
 	self.tiles = [];
 	
+
 	for(var i=0; i<self.numTilesX; i++){
 		self.tiles.push([]);
 		for(var j=0; j<self.numTilesY; j++){
-				self.tiles[i].push(0);
+				self.tiles[i].push({});
 		}
 	}
 
@@ -18,35 +19,75 @@ Map = function(width, height,tile_width, tile_height) {
 	self.placeEntity = function(e){
 		var x = e.x/tile_width;
 		var y = (e.y/tile_height) - offset;
-		console.log("position: ",x,y);
 		var w = x + (e.width/tile_width);
 		var h = y + (e.height/tile_height);
 
-		var filled;
 
 		for(var i=x; i<w; i++){
 			for(var j=y; j<h; j++){
-				filled = (self.tiles[i][j] == 1);
+				filled = !(Object.keys(self.tiles[i][j]).length === 0 && self.tiles[i][j].constructor === Object);
 				if(filled){
 					break;
 				}
-				console.log(i,j);
-				console.log("Filled:", filled);
+					/* do not delete
+					console.log("Cell: ",i,j);
+					console.log("Filled:", filled);
+					*/
+				
 			}
 			if(filled){
-				console.log("Entity already placed in this location");
+				console.log(self.tiles[i][j].type,"already placed in this location");
 				break;
 			}
 		}
 
 		if(!filled){
-			Terrain.add(e);
+			// Terrain.add(e);
 			for(var k=x; k<w; k++){
 				for(var l=y; l<h; l++){
-					self.tiles[k][l] = 1;
+					self.tiles[k][l].id = e.id;
+					self.tiles[k][l].type = e.type;
+
 				}
 			}
-			console.table(self.tiles);
+
+			switch(e.type){
+				case "Terrain":
+					Terrain.add(e);
+					break;
+			}
+		}
+	};
+
+	self.removeEntity = function(x,y){
+		var i = x/tile_width;
+		var j = (y/tile_height)-offset;
+
+		console.log("type",self.tiles[i][j].type);
+
+		switch(self.tiles[i][j].type){
+			case "Terrain":
+				toBeRemoved = Terrain.list[self.tiles[i][j].id];
+				Terrain.remove(toBeRemoved);
+				console.log(Terrain.list);
+				break;
+			default:
+				console.log("Nothing to remove");
+		}	
+
+		self.makeFreeSpace(toBeRemoved);
+	};
+
+	self.makeFreeSpace = function(e){
+		var x = e.x/tile_width;
+		var y = (e.y/tile_height) - offset;
+		var w = x + (e.width/tile_width);
+		var h = y + (e.height/tile_height);
+
+		for(var i=x; i<w; i++){
+			for(var j=y; j<h; j++){
+				self.tiles[i][j] = {};
+			}
 		}
 	};
 
