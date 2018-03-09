eea2f8768cac82217d62ed4b220cf11d9e90454d
ERElli <32199063+ERElli@users.noreply.github.com>
2018-03-09 11:09:22 -0330

    Added code to remove objects from Terrain.list
    Cleaned up code

--- gameCode/levelEditor/Terrain.js
+++ gameCode/levelEditor/Terrain.js
@@ -8,32 +8,29 @@ Terrain = function(id,x,y,width,height){
 		height: height,
 	};
 	
-	//var color = '#'+Math.floor(Math.random()*16777215).toString(16);
-	//self.color = color;
-
 	var terrainImage = new Image();
 	terrainImage.src = "../../images/buildingTerrain3x6.png";
 
 
 	self.draw = function() {
-		 //ctx_lg.fillStyle = self.color;
-		 //ctx_lg.fillRect(self.x,self.y,self.width, self.height);
-
 		ctx_lg.drawImage(terrainImage, self.x, self.y,width,height);
-	}
+	};
 
-	console.log(self);
 	return self;
-}
+};
 
 Terrain.add = function(terrain) {
 	Terrain.list[terrain.id] = terrain;
 	console.log("Adding terrain: ",terrain);
-}
+};
+
+Terrain.remove = function(terrain) {
+	delete Terrain.list[terrain.id];
+};
 
 Terrain.update = function() {
 	for (var key in Terrain.list){
-		var rect = Terrain.list[key];
-		rect.draw();
+		var terrain = Terrain.list[key];
+		terrain.draw();
 	}
-}
+};
