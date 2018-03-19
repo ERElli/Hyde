a112233d899b2d927a6fc9c88ab20322a2a3e54e
John Healey <jph432@mun.ca>
2018-03-19 16:12:08 -0230

    Fix collisions for new coordinate system

--- gameCode/client/engine/entity.js
+++ gameCode/client/engine/entity.js
@@ -32,11 +32,13 @@ function Entity(type, id, x, y, vx, vy, width, height, img, color) {
 	}
 	
 	self.draw = function() {
-		//gui.drawEntity(self);		
+		gui.drawEntity(self);		
+		/*
 		ctx.save();
 		ctx.fillStyle = self.color;
 		ctx.fillRect(self.x-self.width/2,self.y-self.height/2,self.width,self.height);
 		ctx.restore();
+		*/
 	}
 	self.getDistance = function(entity2) {	//return distance (number)
 		var dx = self.x - entity2.x;
@@ -74,10 +76,18 @@ function Entity(type, id, x, y, vx, vy, width, height, img, color) {
 };
 
 var testCollisionRectRect = function(rect1,rect2){
+	
+	return (rect1.x - (rect2.x+rect2.width)) <= 0.1
+			&& (rect2.x - (rect1.x+rect1.width)) <= 0.1
+			&& (rect1.y - (rect2.y+rect2.height)) <= 0.1
+			&& (rect1.y - (rect2.y+rect2.height)) <= 0.1
+			&& (rect2.y - (rect1.y+rect1.height)) <= 0.1
+	/*
 	return rect1.x <= rect2.x+rect2.width 
 		&& rect2.x <= rect1.x+rect1.width
 		&& rect1.y <= rect2.y + rect2.height
 		&& rect2.y <= rect1.y + rect1.height;
+	*/
 };
 
 
