2cba44c7eff1e1518a230030997eaebdf35bd420
ERElli <32199063+ERElli@users.noreply.github.com>
2018-03-22 22:34:06 -0230

    add remove and update methods are now handled by Map.js

--- gameCode/levelEditor/Terrain.js
+++ gameCode/levelEditor/Terrain.js
@@ -11,26 +11,9 @@ Terrain = function(id,x,y,width,height){
 	var terrainImage = new Image();
 	terrainImage.src = "../../images/buildingTerrain3x6.png";
 
-
 	self.draw = function() {
 		ctx_lg.drawImage(terrainImage, self.x, self.y,width,height);
 	};
 
 	return self;
-};
-
-Terrain.add = function(terrain) {
-	Terrain.list[terrain.id] = terrain;
-	console.log("Adding terrain: ",terrain);
-};
-
-Terrain.remove = function(terrain) {
-	delete Terrain.list[terrain.id];
-};
-
-Terrain.update = function() {
-	for (var key in Terrain.list){
-		var terrain = Terrain.list[key];
-		terrain.draw();
-	}
 };
