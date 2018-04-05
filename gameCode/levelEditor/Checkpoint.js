88abac29faad0039b3f636402238863dc4972df4
ERElli <32199063+ERElli@users.noreply.github.com>
2018-04-05 12:51:06 -0230

    updated formatting

--- gameCode/levelEditor/Checkpoint.js
+++ gameCode/levelEditor/Checkpoint.js
@@ -1,36 +1,33 @@
-Checkpoint = function(id,x,y){
-	var self = {
-		id: id,
-		x: x,
-		y: y,
-		width: 50,
-		height: 50,
-	};
+Checkpoint = function(id,type,x,y){
+	var self = {};
+	self.id = id;
+	self.type = type;
+	self.x = x;
+	self.y = y;
+	self.width = 50;
+	self.height = 50;
+	self.triggered = false;
 	
-	console.log(self);
 	return self;
 }
 
-standardCheckpoint = function(id,x,y){
-	var self = Checkpoint(id,x,y);
-	self.type = "standard Checkpoint";
+StandardCheckpoint = function(id,x,y){
+	var self = Checkpoint(id,"standard checkpoint",x,y);
 	self.img = Img.standardCheckpoint;
 
 	self.draw = function(ctx,isLevelEditor){
-		ctx.drawImage(self.img,self.x,self.y, self.img.width, self.img.height);
+		ctx.drawImage(self.img,0,0,self.img.width,self.img.height,self.x,self.y,self.width,self.height);
 	}
 
 	return self;
 }
 
-standardCheckpoint = function(id,x,y){
-	var self = Checkpoint(id,x,y);
-	self.type = "final checkpoint";
+FinalCheckpoint = function(id,x,y){
+	var self = Checkpoint(id,"final checkpoint",x,y);
 	self.img = Img.finalCheckpoint;
 
 	self.draw = function(ctx,isLevelEditor){
 		ctx.drawImage(self.img,self.x,self.y, self.img.width, self.img.height);
 	}
-
 	return self;
 }
