059d1b560eb06a79ed2fc252fefb9e02ce9a2ab1
John Healey <jph432@mun.ca>
2018-04-15 17:39:28 -0230

    Add world 2 level 3

--- gameCode/client/engine/main.js
+++ gameCode/client/engine/main.js
@@ -177,8 +177,15 @@ var doPressedActions = function() {
 * Return true if the given entity is within the renderable radius
 */
 var inRange = function(thing) {
-	len = Math.sqrt( Math.pow(thing.x - player.x, 2) + Math.pow(thing.y - player.y, 2) ) //sqrt(delta_x^2 + delta_y^2)
-	return len < level_width;
+	var len = 0;
+	if (thing.type == 'basic boss' || thing.type == 'flying boss' || thing.type == 'tank boss') {
+		console.log("YEAH");
+		len = 0;
+	}
+	else {
+		len = Math.sqrt( Math.pow(thing.x - player.x, 2) + Math.pow(thing.y - player.y, 2) ) //sqrt(delta_x^2 + delta_y^2)
+	}
+	return len < 1500;
 }
 
 /*
@@ -255,9 +262,9 @@ var update = function() {
 			block.updatePosition();
 		}
 
-		if (!inRange(block)) {
-			continue;
-		}
+		//if (!inRange(block)) {
+			//continue;
+		//}
 
 		//Check collisions with player ####################################
 
@@ -584,6 +591,7 @@ var update = function() {
 		var enemy = enemies[key];
 
 		if (!inRange(enemy)) {
+			console.log("Skipping enemy");
 			continue;
 		}
 
